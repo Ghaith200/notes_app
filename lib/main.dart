a4c13e5c038966ec52ae3b90be1d59a9afb77c9d
Ghaith200 <abdullahghiath2003@gmail.com>
2024-09-21 12:28:17 +0300

    Start point

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Notes App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Notes App'),
+        ),
+        body: InteractiveViewer(
+          panEnabled: true,
+          child: Container(
+            color: Colors.red,
+            child: Text("Notes App",
+                style: Theme.of(context).textTheme.headlineMedium),
+          ),
+        ),
+      ),
+    );
+  }
+}
