534686708f5c053b2e8221f21359b2d2f98dd577
Abdullah Ghaith <abdullahghiath2003@gmail.com>
2024-09-25 00:05:32 +0300

    create edit note view

--- lib/Views/widgets/note_item.dart
+++ lib/Views/widgets/note_item.dart
@@ -1,59 +1,69 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/Views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 20, bottom: 20, left: 16),
-      decoration: BoxDecoration(
-        color: const Color(0xffFFCC80),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              ListTile(
-                title: const Text(
-                  'Note 1',
-                  style: TextStyle(color: Colors.black, fontSize: 26),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(
+          builder: (context) {
+            return const EditNoteView();
+          },
+        ));
+      },
+      child: Container(
+        padding: const EdgeInsets.only(top: 20, bottom: 20, left: 16),
+        decoration: BoxDecoration(
+          color: const Color(0xffFFCC80),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                ListTile(
+                  title: const Text(
+                    'Note 1',
+                    style: TextStyle(color: Colors.black, fontSize: 26),
+                  ),
+                  subtitle: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    child: Text(
+                      'Build Your Career With Abdullah Ghaith',
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        color: Colors.black.withOpacity(0.5),
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                  trailing: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        FontAwesomeIcons.trash,
+                        color: Colors.black,
+                        size: 25,
+                      )),
                 ),
-                subtitle: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 16),
+                Padding(
+                  padding: const EdgeInsets.only(right: 24),
                   child: Text(
-                    'Build Your Career With Abdullah Ghaith',
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
+                    'May21, 2022',
                     style: TextStyle(
-                      color: Colors.black.withOpacity(0.5),
-                      fontSize: 18,
+                      color: Colors.black.withOpacity(0.4),
                     ),
                   ),
                 ),
-                trailing: IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      FontAwesomeIcons.trash,
-                      color: Colors.black,
-                      size: 25,
-                    )),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(right: 24),
-                child: Text(
-                  'May21, 2022',
-                  style: TextStyle(
-                    color: Colors.black.withOpacity(0.4),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ],
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
