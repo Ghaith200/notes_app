aad7b29ec05c806936f65b6aba1da5757be22bc3
Abdullah Ghaith <abdullahghiath2003@gmail.com>
2024-09-25 12:05:30 +0300

    aded  hive package

--- lib/Views/widgets/note_item.dart
+++ lib/Views/widgets/note_item.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:notes_app/Views/edit_note_view.dart';
