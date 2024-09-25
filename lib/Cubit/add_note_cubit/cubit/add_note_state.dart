a8383a7f91a6b847b3c6eaa7fbf5bfe69da3e2ce
Abdullah Ghaith <abdullahghiath2003@gmail.com>
2024-09-26 01:57:44 +0300

    created add note state

--- lib/Cubit/add_note_cubit/cubit/add_note_state.dart
+++ lib/Cubit/add_note_cubit/cubit/add_note_state.dart
@@ -4,3 +4,12 @@ part of 'add_note_cubit.dart';
 sealed class AddNoteState {}
 
 final class AddNoteInitial extends AddNoteState {}
+
+final class AddNoteLoading extends AddNoteState {}
+
+final class AddNoteSucces extends AddNoteState {}
+
+final class AddNoteFailure extends AddNoteState {
+  final String errMessage;
+  AddNoteFailure(this.errMessage);
+}
