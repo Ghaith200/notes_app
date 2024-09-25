0bdce5d22a6d7b66d31c8fba06237c2b347962b3
Abdullah Ghaith <abdullahghiath2003@gmail.com>
2024-09-25 03:07:45 +0300

    added notes model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,15 @@
+class NoteModel {
+  final String title;
+  final String subtitle;
+  final String date;
+  final int color;
+
+  NoteModel(
+      {required this.title,
+      required this.subtitle,
+      required this.date,
+      required this.color});
+
+
+
+}
